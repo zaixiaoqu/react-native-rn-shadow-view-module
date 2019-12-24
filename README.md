53cfb4650773b17d386d62636480ced98fa2f9cb
余胜 <wx_a2f53f9499804ae094c7d8ca7817d653@git.code.tencent.com>
2019-12-24 20:48:34 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# react-native-rn-shadow-view-module
+
+## Getting started
+
+`$ npm install react-native-rn-shadow-view-module --save`
+
+### Mostly automatic installation
+
+`$ react-native link react-native-rn-shadow-view-module`
+
+## Usage
+```javascript
+import RnShadowViewModule from 'react-native-rn-shadow-view-module';
+
+// TODO: What to do with the module?
+RnShadowViewModule;
+```
---
+++ android/src/main/java/com/zaixiaoqu/shadowview/RnShadowViewModulePackage.java
@@ -0,0 +1,23 @@
+package com.zaixiaoqu.shadowview;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+import com.facebook.react.bridge.JavaScriptModule;
+
+public class RnShadowViewModulePackage implements ReactPackage {
+    @Override
+    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
+        return Arrays.<NativeModule>asList(new RnShadowViewModuleModule(reactContext));
+    }
+
+    @Override
+    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+        return Collections.emptyList();
+    }
+}
